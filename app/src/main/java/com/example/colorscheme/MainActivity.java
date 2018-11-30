3fc7ab9439d7020891e82c5022679ea808d07385
Danielle Yang <dy6@illinois.edu>
2018-11-30 17:33:57 -0600

    Started the UI. Began adding onClickListener to getColors button.

--- app/src/main/java/com/example/colorscheme/MainActivity.java
+++ app/src/main/java/com/example/colorscheme/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.colorscheme;
 
+import android.os.Handler;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //TextInputLayout inputSong, inputArtist;
+    Button getColors;
+    String artist, song;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getColors = findViewById(R.id.get_colors);
+        //inputArtist = findViewById(R.id.input_artist);
+        //inputSong = findViewById(R.id.input_song);
+
+        getColors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                artist = findViewById(R.id.input_artist).toString();
+                song = findViewById(R.id.input_song).toString();
+            }
+        });
+
     }
 }
 //nouns & verbs ~1000, adj ~2000, preps ~50, punc ~30
