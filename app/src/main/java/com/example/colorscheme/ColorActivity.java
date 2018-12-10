d67f038eb92ba03cac9458ebb50d654095eaaa62
Danielle Yang <dy6@illinois.edu>
2018-12-10 16:07:29 -0600

    Converts lyrics to hexadecimal color. Having trouble with timing.. line 43 in ColorActivity.

--- app/src/main/java/com/example/colorscheme/ColorActivity.java
+++ app/src/main/java/com/example/colorscheme/ColorActivity.java
@@ -1,32 +1,19 @@
 package com.example.colorscheme;
-
-import android.content.Context;
-import android.os.AsyncTask;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
-import com.android.volley.RequestQueue;
-
-
 
 /**
  * Second page of app, displays color palette created.
  */
 public class ColorActivity extends AppCompatActivity {
-    /** Default logging tag for messages from the main activity. */
-    private static final String TAG = "ColorScheme";
-
-    /** Request queue for our API requests. */
-    private static RequestQueue requestQueue;
-
     /** Message at top of screen. */
     TextView message;
 
-
+    /** color1 */
+    View firstColor;
 
     /**
      * Run when this activity comes to the foreground.
@@ -36,27 +23,15 @@ public class ColorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
-
-
         //text display "Song" by Artist
         message = findViewById(R.id.message);
         message.setText(getIntent().getStringExtra("message"));
 
-//        //API implementation
-//        //https://api.lyrics.ovh/v1/artist/song
-//        String song = getIntent().getStringExtra("song");
-//        String artist = getIntent().getStringExtra("artist");
-
-    }
-    public String getArtist() {
-        message = findViewById(R.id.message);
-        message.setText(getIntent().getStringExtra("message"));
-        return getIntent().getStringExtra("artist");
-    }
-    public String getSong() {
-        message = findViewById(R.id.message);
-        message.setText(getIntent().getStringExtra("message"));
-        return getIntent().getStringExtra("song");
+        // Sets background color of "color1" to hexadecimal value
+        // not working... line 43 runs before the hexa gets a value assigned to it :(
+        firstColor = findViewById(R.id.color1);
+        System.out.println("first color: " + firstColor);
+        firstColor.setBackgroundColor(Color.parseColor("#" + MainActivity.hexa));
     }
 
     /**
--- app/src/main/java/com/example/colorscheme/MainActivity.java
+++ app/src/main/java/com/example/colorscheme/MainActivity.java
@@ -1,31 +1,19 @@
 package com.example.colorscheme;
 
 import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.omt.lyrics.SearchLyrics;
-import com.omt.lyrics.beans.Lyrics;
-import com.omt.lyrics.beans.LyricsServiceBean;
-import com.omt.lyrics.beans.SearchLyricsBean;
-import com.omt.lyrics.exception.SearchLyricsException;
-
-import java.util.List;
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
 /**
  * Main class for app, takes the a song title & artist name.
  */
@@ -39,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
     /** Variable for generate button. */
     Button colorButton;
 
+    /** Request queue for our network requests. */
+    private static RequestQueue requestQueue;
+
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "Color Scheme";
+
+    /** String of lyrics */
+    private static String lyrics;
+
+    /** Hexadecimal representation of lyrics */
+    public static String hexa;
+
     /**
      * Run when this activity comes to the foreground.
      * @param savedInstanceState unused
@@ -47,12 +47,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestQueue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main);
 
         colorButton = findViewById(R.id.color_button);
         artist = findViewById(R.id.input_artist);
         song = findViewById(R.id.input_song);
-
+        final Button startAPICall = findViewById(R.id.color_button);
         colorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,30 +61,49 @@ public class MainActivity extends AppCompatActivity {
                 //for the API implementation
                 intent.putExtra("song", song.getText().toString());
                 intent.putExtra("artist", artist.getText().toString());
-
-//                //for the heading on colorActivity page and to print out lyrics
+                // start API call and display lyrics
+                startAPICall();
+                //for the heading on colorActivity page and to print out lyrics
                 intent.putExtra("message", song.getText().toString() + " by " + artist.getText().toString());
-//                + "\n"
-//                        + getLyrics());
-
-
                 startActivity(intent);
-                MyAsyncTask asyncTask = new MyAsyncTask();//
-                /* new MyAsyncTask(new MyAsyncTask.AsyncResponse() {
 
-                    //@Override
-                    public void processFinish(String output) {
-                        Log.d("Async task response: ", (output));
-                        colorButton.setText(output);
-                    }
-                });*/
-                try {
-                    asyncTask.startAPICall(song.getText().toString(), artist.getText().toString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                //asyncTask.execute();
             }
         });
+    }
+
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    //"https://api.lyrics.ovh/v1/\" + artist + \"/\" + song + \"/json",
+                    "https://api.lyrics.ovh/v1/Adele/Hello",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                            setLyricsAndHexa(response.toString());
+                            System.out.println(lyrics);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    /**
+     * Sets the lyrics
+     * @param inputLyrics the lyrics
+     */
+    public void setLyricsAndHexa(String inputLyrics) {
+        lyrics = inputLyrics;
+        hexa = Integer.toHexString(lyrics.hashCode()).toUpperCase();
+        System.out.println("Hexa: " + hexa);
     }
 }
