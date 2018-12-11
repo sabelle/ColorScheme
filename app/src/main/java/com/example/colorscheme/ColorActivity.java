8a91a60e1924df13a54610c5fc3a296533a2f144
Danielle Yang <dy6@illinois.edu>
2018-12-11 14:12:48 -0600

    WORKS!!! Start colorActivity in callback onResponse method. Capitalize the first letter in the song title and artist name. Handled errors!! :) :)

--- app/src/main/java/com/example/colorscheme/ColorActivity.java
+++ app/src/main/java/com/example/colorscheme/ColorActivity.java
@@ -12,8 +12,26 @@ public class ColorActivity extends AppCompatActivity {
     /** Message at top of screen. */
     TextView message;
 
-    /** color1 */
+    /** Message displayed if error occurs */
+    TextView error;
+
+    /** View component for color1 */
     View firstColor;
+
+    /** View component for color2 */
+    View secondColor;
+
+    /** View component for color3 */
+    View thirdColor;
+
+    /** color1 as an int */
+    int color1;
+
+    /** color2 as an int */
+    int color2;
+
+    /** color3 as an int */
+    int color3;
 
     /**
      * Run when this activity comes to the foreground.
@@ -25,15 +43,44 @@ public class ColorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_color);
         //text display "Song" by Artist
         message = findViewById(R.id.message);
-        message.setText(getIntent().getStringExtra("message"));
+        error = findViewById(R.id.error_view);
 
-        // Sets background color of "color1" to hexadecimal value
-        // not working... line 43 runs before the hexa gets a value assigned to it :(
+        message.setText(getIntent().getStringExtra("message"));
+        error.setText(getIntent().getStringExtra("error"));
+        //access colors
         firstColor = findViewById(R.id.color1);
-        System.out.println("first color: " + firstColor);
-        firstColor.setBackgroundColor(Color.parseColor("#" + MainActivity.hexa));
+        secondColor = findViewById(R.id.color2);
+        thirdColor = findViewById(R.id.color3);
+        try {
+            setColors();
+            firstColor.setBackgroundColor(color1);
+            secondColor.setBackgroundColor(color2);
+            thirdColor.setBackgroundColor(color3);
+        } catch (Exception e) { }
     }
 
+    /**
+     * Sets the value of the colors
+     * Handles hexadecimal values with 7 characters instead of 8.
+     * Turns them into 6-value hexadecimal representations of color (instead of 8).
+     */
+    public void setColors() {
+        if (MainActivity.hexa1.length() == 8) {
+            color1 = Color.parseColor(MainActivity.hexa1.substring(0, 7));
+        } else {
+            color1 = Color.parseColor(MainActivity.hexa1);
+        }
+        if (MainActivity.hexa2.length() == 8) {
+            color2 = Color.parseColor(MainActivity.hexa2.substring(0, 7));
+        } else {
+            color2 = Color.parseColor(MainActivity.hexa2);
+        }
+        if (MainActivity.hexa3.length() == 8) {
+            color3 =Color.parseColor(MainActivity.hexa3.substring(0, 7));
+        } else {
+            color3 = Color.parseColor(MainActivity.hexa3);
+        }
+    }
     /**
      * Run when this activity is no longer visible.
      */
--- app/src/main/java/com/example/colorscheme/MainActivity.java
+++ app/src/main/java/com/example/colorscheme/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.colorscheme;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,9 +13,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONObject;
+
 /**
  * Main class for app, takes the a song title & artist name.
  */
@@ -27,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
     /** Variable for generate button. */
     Button colorButton;
 
+    /** Song title as a string, and capitalized */
+    String strSong;
+
+    /** Song artist as a string, and capitalized */
+    String strArtist;
+
     /** Request queue for our network requests. */
     private static RequestQueue requestQueue;
 
@@ -37,13 +47,18 @@ public class MainActivity extends AppCompatActivity {
     private static String lyrics;
 
     /** Hexadecimal representation of lyrics */
-    public static String hexa;
+    public static String hexa1;
+
+    /** Hexadecimal representation of song title */
+    public static String hexa2;
+
+    /** Hexadecimal representation of song artist */
+    public static String hexa3;
 
     /**
      * Run when this activity comes to the foreground.
      * @param savedInstanceState unused
      */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,41 +68,42 @@ public class MainActivity extends AppCompatActivity {
         colorButton = findViewById(R.id.color_button);
         artist = findViewById(R.id.input_artist);
         song = findViewById(R.id.input_song);
+
         final Button startAPICall = findViewById(R.id.color_button);
         colorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ColorActivity.class);
-                //for the API implementation
-                intent.putExtra("song", song.getText().toString());
-                intent.putExtra("artist", artist.getText().toString());
-                // start API call and display lyrics
+                //reformat song and artist to have uppercase first letters of each word
+                strSong = song.getText().toString();
+                strArtist = artist.getText().toString();
+                reformatInput(strSong, "song");
+                reformatInput(strArtist, "artist");
+                //start API call
                 startAPICall();
-                //for the heading on colorActivity page and to print out lyrics
-                intent.putExtra("message", song.getText().toString() + " by " + artist.getText().toString());
-                startActivity(intent);
-
             }
         });
     }
 
+    /** Call the lyrics API */
     void startAPICall() {
         try {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
                     //"https://api.lyrics.ovh/v1/\" + artist + \"/\" + song + \"/json",
-                    "https://api.lyrics.ovh/v1/Adele/Hello",
+                    "https://api.lyrics.ovh/v1/" + strArtist + "/" + strSong,
                     null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(final JSONObject response) {
                             Log.d(TAG, response.toString());
                             setLyricsAndHexa(response.toString());
-                            System.out.println(lyrics);
                         }
                     }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(final VolleyError error) {
+                    Intent errorIntent = new Intent(MainActivity.this, ColorActivity.class);
+                    errorIntent.putExtra("error", "Error: No lyrics found. Please double check the spelling of the song title and artist.");
+                    startActivity(errorIntent);
                     Log.w(TAG, error.toString());
                 }
             });
@@ -95,15 +111,40 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
     /**
      * Sets the lyrics
      * @param inputLyrics the lyrics
      */
     public void setLyricsAndHexa(String inputLyrics) {
+        Intent intent = new Intent(MainActivity.this, ColorActivity.class);
+        intent.putExtra("message", strSong + " by " + strArtist);
         lyrics = inputLyrics;
-        hexa = Integer.toHexString(lyrics.hashCode()).toUpperCase();
-        System.out.println("Hexa: " + hexa);
+        hexa1 = "#" +  Integer.toHexString(song.getText().toString().hashCode()).toUpperCase();
+        hexa2 = "#" + Integer.toHexString(artist.getText().toString().hashCode()).toUpperCase();
+        hexa3 = "#" + Integer.toHexString(lyrics.hashCode()).toUpperCase();
+        startActivity(intent);
+    }
+    /**
+     * Reformats the song and artist name. The first letter of each word is capitalized.
+     * @param input the input string, either the song name or artist name
+     * @param type the type of string (song or artist)
+     */
+    public void reformatInput(String input, String type) {
+        for (int i = 0; i < input.length() - 2; i++) {
+            if (i == 0) {
+                input = input.substring(0, 1).toUpperCase()
+                        + input.substring(1);
+            } else if (input.substring(i, i + 1).equals(" ")) {
+                input = input.substring(0, i + 1)
+                        + input.substring(i + 1, i + 2).toUpperCase()
+                        + input.substring(i + 2);
+            }
+        }
+        if (type.equals("song")) {
+            strSong = input;
+        } else {
+            strArtist = input;
+        }
     }
 }
